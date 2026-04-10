e4c171471697493d43ec3a2a3f17acdaddc068a0
GribTheGrumlin <alpiheiskanen09@gmail.com>
2026-04-10 09:38:07 +0300

    vydfvdfvbh

--- Rust&Bust/Assets/Scripts/BotSpawner.cs
+++ Rust&Bust/Assets/Scripts/BotSpawner.cs
@@ -7,7 +7,9 @@ public class BotSpawner : MonoBehaviour
 
     public void SpawnBot()
     {
-        Instantiate(botPrefab, transform.position, Quaternion.identity);
+        //Instantiate(botPrefab, transform.position, Quaternion.identity);
+        Vector3 spawnPos = transform.position + Vector3.up * 10f;
+        Instantiate(botPrefab, spawnPos, Quaternion.identity);
     }
 
     public void RespawnBot()
